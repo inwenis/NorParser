dbb034d6272ba23975a091a9268d89e466b6118d
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 14:29:44 +0100

    add simple CsvWriter

---
+++ src/Logic.Tests/CsvWriterTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Logic;
+using Machine.Specifications;
+
+[Subject(typeof(CsvWriter))]
+class Serializing_simple_sentence_to_csv
+{
+    Establish context = () =>
+    {
+        input = new List<Sentence>
+        {
+            new Sentence {Words = new List<string> {"a", "bb", "ccc"}}
+        };
+        sut = new CsvWriter();
+    };
+
+    Because of = () =>
+    {
+        result = sut.Write(input);
+    };
+
+    It returns_all_words_from_input = () =>
+    {
+        result.ShouldContain("a");
+        result.ShouldContain("bb");
+        result.ShouldContain("ccc");
+    };
+
+    static CsvWriter sut;
+    static List<Sentence> input;
+    static string result;
+}
---
+++ src/Logic/CsvWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public class CsvWriter
+    {
+        public string Write(List<Sentence> sentences)
+        {
+            var sb = new StringBuilder();
+            foreach (var sentence in sentences)
+            {
+                sb.AppendLine(string.Join(", ", sentence.Words));
+            }
+            return sb.ToString();
+        }
+    }
+}
