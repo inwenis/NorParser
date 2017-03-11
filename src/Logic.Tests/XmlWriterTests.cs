36fb97987e834de0f2674ddbac8e1bf1e29326c6
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 15:58:56 +0100

    add test for multiple sentences

--- src/Logic.Tests/XmlWriterTests.cs
+++ src/Logic.Tests/XmlWriterTests.cs
@@ -41,3 +41,31 @@ class Serializing_simple_sentence
     static List<Sentence> input;
     static XDocument result;
 }
+
+[Subject(typeof(XmlWriter))]
+class Serializing_multiple_sentences
+{
+    Establish context = () =>
+    {
+        input = new List<Sentence>
+        {
+            new Sentence {Words = new List<string> {"a", "bb", "ccc"}},
+            new Sentence {Words = new List<string> {"d", "ee", "fff"}}
+        };
+        sut = new XmlWriter();
+    };
+
+    Because of = () =>
+    {
+        result = sut.Write(input);
+    };
+
+    It returnes_xml_with_element_for_each_sentence = () =>
+    {
+        result.XPathSelectElements("/text/sentence").Count().ShouldEqual(2);
+    };
+
+    static XmlWriter sut;
+    static List<Sentence> input;
+    static XDocument result;
+}
