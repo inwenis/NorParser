2b15d3f993a891e0cd9f30cf3858165756468e95
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 13:11:58 +0100

    refactor

--- src/Logic/Parser.cs
+++ src/Logic/Parser.cs
@@ -7,24 +7,26 @@ namespace Logic
 {
     public class Parser
     {
+        private readonly char[] _sentenceSeparators = { '.' };
+
         public List<Sentence> Parse(string input)
         {
-            var list = new List<Sentence>();
-            var sentences = input.Split(new [] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var parsedSentences = new List<Sentence>();
+            var sentences = input.Split(_sentenceSeparators, StringSplitOptions.RemoveEmptyEntries);
             foreach (var sentence in sentences)
             {
-                var words = ReplaceNonAlphabetCharactersWithSpace(sentence)
-                    .Split(' ')
+                var words = ReplaceCharactersNotAllowedInWordsWithSpaces(sentence)
+                    .Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries)
                     .Where(w => !string.IsNullOrWhiteSpace(w))
                     .Where(w => !w.All(char.IsPunctuation))
                     .OrderBy(s => s)
                     .ToList();
-                list.Add(new Sentence {Words = words});
+                parsedSentences.Add(new Sentence {Words = words});
             }
-            return list;
+            return parsedSentences;
         }
 
-        private string ReplaceNonAlphabetCharactersWithSpace(string input)
+        private string ReplaceCharactersNotAllowedInWordsWithSpaces(string input)
         {
             return Regex.Replace(input, "[^a-zA-Z'-]+", " ");
         }
