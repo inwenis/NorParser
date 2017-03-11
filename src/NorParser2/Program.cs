e20f1effca45489f12b71788aeeeb34af180732a
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 12:58:15 +0100

    add input from spect for test runs

--- src/NorParser2/Program.cs
+++ src/NorParser2/Program.cs
@@ -10,7 +10,40 @@ namespace NorParser2
     {
         public static void Main(string[] args)
         {
-            var output = new Parser().Parse("  had ~ ! @ # $ % ^ & * ( ) _ + - = ` { } |  : \" < > ? [ ] \\ ; ' , . / * - + , ");
+            var parser = new Parser();
+            var inputSimple = "Mary had a little lamb. Peter called for the wolf, and Aesop came. Cinderella likes shoes.";
+            var inputWithNewLinesAndExcessiveSpaces =
+                "  Mary   had a little  lamb  . \n" +
+                "\n" +
+                "\n" +
+                "Peter called for the wolf, and Aesop came." +
+                "Cinderella  likes shoes.";
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(inputSimple);
+            foreach (var sentence in parser.Parse(inputSimple))
+            {
+                Console.WriteLine("sentence:");
+                foreach (var word in sentence.Words)
+                {
+                    Console.WriteLine($"\t{word}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(inputWithNewLinesAndExcessiveSpaces);
+            foreach (var sentence in parser.Parse(inputWithNewLinesAndExcessiveSpaces))
+            {
+                Console.WriteLine("sentence:");
+                foreach (var word in sentence.Words)
+                {
+                    Console.WriteLine($"\t{word}");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
