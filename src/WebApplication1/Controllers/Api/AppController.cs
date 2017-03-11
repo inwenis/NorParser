73b0a5daedca59d45b6c2dad281abf8ee87ffa37
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 15:26:36 +0100

    add simple WebApi

---
+++ src/WebApplication1/Controllers/Api/AppController.cs
@@ -0,0 +1,28 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers.Api
+{
+    public class AppController : Controller
+    {
+        [HttpPost("api/toXml")]
+        public IActionResult ToXml(string text)
+        {
+            var parser = new Parser();
+            var sentences = parser.Parse(text);
+            var xmlWriter = new XmlWriter();
+            var xDocument = xmlWriter.Write(sentences);
+            return Content(xDocument.ToString());
+        }
+
+        [HttpPost("api/toCsv")]
+        public IActionResult ToCsv(string text)
+        {
+            var parser = new Parser();
+            var sentences = parser.Parse(text);
+            var csvWriter = new CsvWriter();
+            var csv = csvWriter.Write(sentences);
+            return Content(csv);
+        }
+    }
+}
