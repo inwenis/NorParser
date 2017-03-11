a108df526ddf497b88fd69b193c4aa18f06fd66c
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-11 14:45:01 +0100

    add simple controller and view using Logic classes
    this creates a dancing skeleton

---
+++ src/WebApplication1/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult ConvertToXml(string text)
+        {
+            var parser = new Parser();
+            var sentences = parser.Parse(text);
+            var xmlWriter = new XmlWriter();
+            var xDocument = xmlWriter.Write(sentences);
+            ViewData["Xml"] = xDocument.ToString();
+            return View();
+        }
+    }
+}
