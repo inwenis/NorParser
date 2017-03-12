42e985047df73172fddc4c602d64af9ec5eadd22
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-12 12:10:30 +0100

    remove uploading text files

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using Logic;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
@@ -10,46 +7,6 @@ namespace WebApplication1.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult ConvertToXml(string text)
-        {
-            var parser = new Parser();
-            var sentences = parser.Parse(text);
-            var xmlWriter = new XmlWriter();
-            var xDocument = xmlWriter.Write(sentences);
-            ViewData["ConversionResult"] = xDocument.ToString();
-            return View("Index");
-        }
-
-        public IActionResult TextFileToXml(IFormFile textFile)
-        {
-            string text;
-            using (var reader = new StreamReader(textFile.OpenReadStream()))
-            {
-                text = reader.ReadToEnd();
-            }
-            return ConvertToXml(text);
-        }
-
-        public IActionResult ConvertToCsv(string text)
-        {
-            var parser = new Parser();
-            var sentences = parser.Parse(text);
-            var csvWriter = new CsvWriter();
-            var csv = csvWriter.Write(sentences);
-            ViewData["ConversionResult"] = csv;
-            return View("Index");
-        }
-
-        public IActionResult TextFileToCsv(IFormFile textFile)
-        {
-            string text;
-            using (var reader = new StreamReader(textFile.OpenReadStream()))
-            {
-                text = reader.ReadToEnd();
-            }
-            return ConvertToCsv(text);
         }
 
         public IActionResult Error()
