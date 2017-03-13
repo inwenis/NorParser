e8b52720d5ee5cad0a37f933d04cabce54a1328e
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-13 23:30:29 +0100

    escape quotes and slashed before sending string
    thanks to this double and single quotes and slashes can be used in input

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -1,13 +1,18 @@
-﻿function SendRequest(uri, data, callback) {
+﻿function EscapeQuotesAndSlashes(string) {
+    return (string + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+
+function SendRequest(uri, data, callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             callback(httpRequest.responseText);
         }
     }
+    var dataWithEscapedCharacters = EscapeQuotesAndSlashes(data);
     httpRequest.open("POST", uri, true);
     httpRequest.setRequestHeader("Content-Type", "application/json");
-    httpRequest.send("\"" + data + "\"");
+    httpRequest.send("\"" + dataWithEscapedCharacters + "\"");
 }
 
 function DisplayResult(text) {
