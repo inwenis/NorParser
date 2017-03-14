132479ec6b1ddd64cb44548e2ad641f675de91b2
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-03-14 12:31:12 +0100

    use strict mode, use immidiately invoked function
    separete html view from js logic - move registering to onclick event to js

--- src/NorParser.WebApp/wwwroot/js/site.js
+++ src/NorParser.WebApp/wwwroot/js/site.js
@@ -1,30 +1,37 @@
-﻿function EscapeQuotesAndSlashes(string) {
-    return (string + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-}
-
-function SendRequest(uri, data, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-            callback(httpRequest.responseText);
-        }
+﻿(function () {
+    "use strict";
+    function EscapeQuotesAndSlashes(string) {
+        return (string + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
     }
-    var dataWithEscapedCharacters = EscapeQuotesAndSlashes(data);
-    httpRequest.open("POST", uri, true);
-    httpRequest.setRequestHeader("Content-Type", "application/json");
-    httpRequest.send("\"" + dataWithEscapedCharacters + "\"");
-}
 
-function DisplayResult(text) {
-    document.getElementById("resultTextArea").innerText = text;
-}
+    function SendRequest(uri, data, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                callback(httpRequest.responseText);
+            }
+        }
+        var dataWithEscapedCharacters = EscapeQuotesAndSlashes(data);
+        httpRequest.open("POST", uri, true);
+        httpRequest.setRequestHeader("Content-Type", "application/json");
+        httpRequest.send("\"" + dataWithEscapedCharacters + "\"");
+    }
 
-function ConvertToXml() {
-    var input = document.getElementById("input").value;
-    SendRequest("/api/toXml", input, DisplayResult);
-}
+    function DisplayResult(text) {
+        document.getElementById("resultTextArea").innerText = text;
+    }
 
-function ConvertToCsv() {
-    var input = document.getElementById("input").value;
-    SendRequest("/api/toCsv", input, DisplayResult);
-}
+    function ConvertToXml() {
+        var input = document.getElementById("input").value;
+        SendRequest("/api/toXml", input, DisplayResult);
+    }
+
+    function ConvertToCsv() {
+        var input = document.getElementById("input").value;
+        SendRequest("/api/toCsv", input, DisplayResult);
+    }
+
+    document.getElementById("toXmlButton").onclick = ConvertToXml;
+    document.getElementById("toCsvButton").onclick = ConvertToCsv;
+
+})();
